087161f748851140aa74d6c46da659cd2fe44889
bengin34 <becaglar3434@gmail.com>
2024-03-06 15:26:41 +0100

    Hero section final

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,7 +1,7 @@
 import SectionSvg from "../assets/svg/SectionSvg";
 
 const Section = ({
-  classNames,
+  className,
   id,
   crosses,
   crossesOffset,
@@ -11,17 +11,19 @@ const Section = ({
   return (
     <div
       id={id}
-      className={`relative ${
+      className={`
+      relative 
+      ${
         customPaddings ||
-        `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}
-        ${classNames || ""}`
-      }`}
+        `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
+      } 
+      ${className || ""}`}
     >
       {children}
 
-      <div className="absolute top-0 left-5 w-0.25 h-full hidden bg-stroke-1 pointer-events-none md:block lg:left-7.5 xl:left-10" />
+      <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:left-7.5 xl:left-10" />
+      <div className="hidden absolute top-0 right-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:right-7.5 xl:right-10" />
 
-      <div className="absolute top-0 left-5 w-0.25 h-full hidden bg-stroke-1 pointer-events-none md:block lg:right-7.5 xl:right-10" />
       {crosses && (
         <>
           <div
