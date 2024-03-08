20887c12b7f3a74ee3c2fdb9dcb5c0e30b7f02fd
bengin34 <becaglar3434@gmail.com>
2024-03-08 12:46:27 +0100

    Fix font sizes

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -21,7 +21,7 @@ const Benefits = () => {
               style={{ backgroundImage: `url(${item.backgroundUrl})` }}
             >
               <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
-                <h5 className="h2 mb-5">{item.title}</h5>
+                <h5 className="h5 mb-5">{item.title}</h5>
                 <p className="body-2 mb-2 text-n-3">{item.text}</p>
                 <div className="flex items-center mt-auto">
                   <img
